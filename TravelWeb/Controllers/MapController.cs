829acad33d2a256e263de6892dff19b68cad2635
chuatehaonam <hihi36hihi@gmail.com>
2025-10-19 15:18:46 +0700

    Add Maps
    Maps before update

---
+++ TravelWeb/Controllers/MapController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelWeb.Controllers
+{
+    public class MapController : Controller
+    {
+        // /Map
+        public IActionResult Index(string city, double lat, double lng)
+        {
+            ViewBag.City = city;
+            ViewBag.Lat = lat;
+            ViewBag.Lng = lng;
+            return View();
+        }
+    }
+}
