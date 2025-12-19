636239c4b5ad998fd7667ac2b4a156059f6a6bba
chuatehaonam <hihi36hihi@gmail.com>
2025-12-19 19:27:35 +0700

    allin

--- TravelWeb/obj/Debug/net8.0/TravelWeb.AssemblyInfo.cs
+++ TravelWeb/obj/Debug/net8.0/TravelWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4381bfd678e642b7a2c6ea27255e7c5e6a70c29c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d022c2290d34ceb1ce9835d4dc7ca65833a90e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
